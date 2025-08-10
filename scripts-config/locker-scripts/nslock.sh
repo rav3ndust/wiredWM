a014a536bc8f28f0caaee5b9396eeaa6e1a51950
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2025-08-10 06:23:12 -0500

    Update nslock.sh

--- scripts-config/locker-scripts/nslock.sh
+++ scripts-config/locker-scripts/nslock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # nslock
 # a simple locker for wiredWM (wayland) which uses swaylock and a custom image and the nightshadeNeon color scheme.
-img="$HOME/wiredWM/wp/lain-machine.png"
+img="$HOME/wiredWM/wp/all-love-lain.jpeg"
 ringcolor="#ff10f0"
 ringvercolor="#39ff14"
 textvercolor="#ffffff"
