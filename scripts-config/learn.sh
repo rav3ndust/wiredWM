d27d34844d43fcd07ec1d6b7f32f7735535e1c18
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2025-08-12 18:03:06 -0500

    Update learn.sh

--- scripts-config/learn.sh
+++ scripts-config/learn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Display keybindings from learn.txt in rofi and show selection in less
+# Display our helpful reference manual, 'learn', using a keybinding (Super+Shift+H)
+# Helps the user learn how to use wiredWM with built-in documentation.  
 learning_doc="$HOME/wiredWM/scripts-config/learn.txt"
 foot bash -c "less $learning_doc"
