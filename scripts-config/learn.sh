637ac03973a6edf65457904b280bb425eab6d96a
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2025-09-05 19:00:26 -0500

    Update learn.sh

--- scripts-config/learn.sh
+++ scripts-config/learn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Display our helpful reference manual, 'learn', using a keybinding (Super+Shift+H)
 # Helps the user learn how to use wiredWM with built-in documentation.  
-learning_doc="$HOME/wiredWM/scripts-config/learn.txt"
-foot bash -c "less $learning_doc"
+help="manual/manual.html"
+foot -e elinks $help
